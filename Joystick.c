7c5966df9405eaa46d8b9bafd0084194b7840b7a
Joe <joepshoulak2@gmail.com>
2020-09-04 17:33:02 -0500

    Undid Bike Boosting, but made other improvements
    Bike boosting was undone because the B button was interfering with the egg hatching.

--- Joystick.c
+++ Joystick.c
@@ -111,15 +111,15 @@ static const command step[] = {
 	
 	// Open Pokemon Boxes
 	{ X,         5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  30 },
 	{ A,         5 },
 	{ NOTHING,  60 },
 	{ R,         5 },
-	{ NOTHING, 150 },
+	{ NOTHING, 100 },
 	
 	// Multiselect
 	{ Y,         5 },
-	{ NOTHING,  15 },
+	{ NOTHING,   5 },
 	{ Y,         5 },
 	
 	// Grab 5 eggs
@@ -141,15 +141,14 @@ static const command step[] = {
 	{ A,         5 },
 	{ LEFT,      5 },
 	{ A,         5 },
-	{ NOTHING, 100 },
 	
 	// Exit Menu
 	{ B,         5 },
-	{ NOTHING, 100 },
+	{ NOTHING,  75 },
 	{ B,         5 },
-	{ NOTHING, 100 },
+	{ NOTHING,  75 },
 	{ B,         5 },
-	{ NOTHING, 100 },
+	{ NOTHING,  75 },
 	
 	//// HATCH EGGS ////
 	
@@ -157,123 +156,104 @@ static const command step[] = {
 	{ LEFT,      5 },
 	{ NOTHING,  15 },
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  15 },
 		
 	// Left
-	{ LEFT,     10 },
-	{ L_BOOST, 350 },
-	{ LEFT,     10 },
-	{ L_BOOST, 350 },
-	{ NOTHING,  50 },
+	{ LEFT,    750 },
+	{ NOTHING,  15 },
 
 	// Face Right
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  25 },
 	{ RIGHT,     5 },
 	{ NOTHING,  15 },
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  15 },
 		
 	// Right
-	{ RIGHT,    10 },
-	{ R_BOOST, 350 },
-	{ RIGHT,    10 },
-	{ R_BOOST, 400 },
-	{ NOTHING,  50 },
+	{ RIGHT,   725 },
+	{ NOTHING,  15 },
 
 	// Face left
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  25 },
 	{ LEFT,      5 },
 	{ NOTHING,  15 },
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  15 },
 		
 	// Left
-	{ LEFT,     10 },
-	{ L_BOOST, 350 },
-	{ LEFT,     10 },
-	{ L_BOOST, 350 },
-	{ NOTHING,  50 },
+	{ LEFT,    725 },
+	{ NOTHING,  15 },
 
 	// Face Right
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  25 },
 	{ RIGHT,     5 },
 	{ NOTHING,  15 },
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  15 },
 		
 	// Right
-	{ RIGHT,    10 },
-	{ R_BOOST, 350 },
-	{ RIGHT,    10 },
-	{ R_BOOST, 400 },
-	{ NOTHING,  50 },
+	{ RIGHT,   750 },
+	{ NOTHING,  15 },
 
 	// Face Left
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  25 },
 	{ LEFT,      5 },
 	{ NOTHING,  15 },
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  15 },
 		
 	// Left
-	{ LEFT,     10 },
-	{ L_BOOST, 350 },
-	{ LEFT,     10 },
-	{ L_BOOST, 350 },
-	{ NOTHING,  50 },
+	{ LEFT,    725 },
+	{ NOTHING,  15 },
 
 	// Face Right
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  25 },
 	{ RIGHT,     5 },
 	{ NOTHING,  15 },
 	{ PLUS,      5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  15 },
 		
 	// Right
-	{ RIGHT,    10 },
-	{ R_BOOST, 350 },
-	{ RIGHT,    10 },
-	{ R_BOOST, 400 },
+	{ RIGHT,   750 },
+	{ NOTHING,  15 },
 	
 	// Hatch 5 Eggs	
+	{ A,         5 },
+	{ NOTHING, 750 },
+	{ A,         5 },
+	{ NOTHING, 100 },
+	{ RIGHT,     5 },
+	{ NOTHING,   5 },
 
 	{ A,         5 },
-	{ NOTHING, 800 },
+	{ NOTHING, 750 },
 	{ A,         5 },
-	{ NOTHING, 200 },
-	{ RIGHT,    10 },
-	{ NOTHING,  50 },
+	{ NOTHING, 100 },
+	{ RIGHT,     5 },
+	{ NOTHING,   5 },
 
 	{ A,         5 },
-	{ NOTHING, 800 },
+	{ NOTHING, 750 },
 	{ A,         5 },
-	{ NOTHING, 200 },
-	{ RIGHT,    10 },
-	{ NOTHING,  50 },
-
-	{ A,         5 },
-	{ NOTHING, 800 },
-	{ A,         5 },
-	{ NOTHING, 200 },
-	{ RIGHT,    10 },
-	{ NOTHING,  50 },
+	{ NOTHING, 100 },
+	{ RIGHT,     5 },
+	{ NOTHING,   5 },
 	
 	{ A,         5 },
-	{ NOTHING, 800 },
+	{ NOTHING, 750 },
 	{ A,         5 },
-	{ NOTHING, 200 },
-	{ RIGHT,    10 },
-	{ NOTHING,  50 },
+	{ NOTHING, 100 },
+	{ RIGHT,     5 },
+	{ NOTHING,   5 },
 
 	{ A,         5 },
-	{ NOTHING, 800 },
+	{ NOTHING, 750 },
 	{ A,         5 },
-	{ NOTHING,  50 }, 
 	
 	// Right in case this command was missed because eggs were ready
 	{ RIGHT,    10 },
@@ -289,126 +269,119 @@ static const command step[] = {
 	
 	// Open Pokemon Boxes
 	{ X,         5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  30 },
 	{ A,         5 },
 	{ NOTHING,  60 },
 	{ R,         5 },
-	{ NOTHING, 150 },
+	{ NOTHING, 100 },
 	
 	// Multiselect
 	{ Y,         5 },
-	{ NOTHING,  15 },
+	{ NOTHING,  10 },
 	{ Y,         5 },
 	
 	// Grab party
 	{ LEFT,      5 },
-	{ NOTHING,  20 },
 	{ DOWN,      5 },
-	{ NOTHING,  20 },
 	{ A,         5 },
-	{ NOTHING,  20 },
+	{ NOTHING,  10 },
 	{ UP,        5 },
-	{ NOTHING,  20 },
+	{ NOTHING,  10 },
 	{ UP,        5 },
-	{ NOTHING,  20 },
 	{ A,         5 },
-	{ NOTHING,  20 },
 	
 	// Place in correct row
 	{ LEFT,      5 },
-	{ NOTHING,  20 },
 	{ UP,        5 },
-	{ NOTHING,  20 },
 	{ A,         5 },
-	{ NOTHING,  20 },
 		
 	// View Boxes
 	{ UP,        5 },
-	{ NOTHING,  20 },
+	{ NOTHING,   5 },
 	{ UP,        5 },
-	{ NOTHING,  20 },
+	{ NOTHING,   5 },
 	{ LEFT,      5 },
-	{ NOTHING,  20 },
+	{ NOTHING,   5 },
 	{ A,         5 },
-	{ NOTHING,  50 },
+	{ NOTHING,  25 },
 	
 	// Shift Boxes Right
 	{ Y,         5 },
 	{ RIGHT,     5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ RIGHT,     5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ RIGHT,     5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ RIGHT,     5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ RIGHT,     5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ RIGHT,     5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ RIGHT,     5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 	
 	// Shift Box Down One
 	{ Y,         5 },
 	{ DOWN,      5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 	
 	// Shift Boxes Left
 	{ Y,         5 },
 	{ LEFT,      5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ LEFT,      5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ LEFT,      5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ LEFT,      5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ LEFT,      5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ LEFT,      5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 
 	{ Y,         5 },
 	{ LEFT,      5 },
 	{ Y,         5 },
-	{ NOTHING,  10 },
+	{ NOTHING,   5 },
 	
 	// End on the first box
 	{ UP,        5 },
@@ -418,14 +391,12 @@ static const command step[] = {
 			
 	// Exit Menu
 	{ B,         5 },
-	{ NOTHING, 150 },
+	{ NOTHING,  75 },
 	{ B,         5 },
-	{ NOTHING, 150 },
+	{ NOTHING,  75 },
 	{ R,         5 },  // Save on the way out
-	{ NOTHING, 150 },
+	{ NOTHING,  50 },
 	{ A,         5 },
-	{ NOTHING, 150 },
-	{ B,         5 },
 	{ NOTHING, 150 },
 };
 
